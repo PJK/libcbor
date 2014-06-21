a36534a1726620c33645032bf035561e69aad0e6
PJK <me@pavelkalvoda.com>
2014-06-21 23:43:49 +0200

    Tests formatting

--- test/type_0_test.c
+++ test/type_0_test.c
@@ -18,107 +18,107 @@ unsigned char data5[] = { 0x1b, 0xa5, 0xf7, 0x02, 0xb3, 0xa5, 0xf7, 0x02, 0xb3, 
 
 
 static void test_very_short_int(void **state) {
-  number = cbor_load(data1, 2, 0, &res);
-  assert_true(cbor_typeof(number) == CBOR_TYPE_UINT);
-  assert_true(cbor_uint_get_width(number) == CBOR_INT_8);
-  assert_true(cbor_isa_uint(number));
-  assert_false(cbor_isa_negint(number));
-  assert_true(cbor_get_uint8(number) == 2);
-  assert_true(res.error.code == 0);
-  assert_true(res.read == 1);
-  assert_true(cbor_is_int(number));
-  cbor_decref(&number);
-  assert_null(number);
+	number = cbor_load(data1, 2, 0, &res);
+	assert_true(cbor_typeof(number) == CBOR_TYPE_UINT);
+	assert_true(cbor_uint_get_width(number) == CBOR_INT_8);
+	assert_true(cbor_isa_uint(number));
+	assert_false(cbor_isa_negint(number));
+	assert_true(cbor_get_uint8(number) == 2);
+	assert_true(res.error.code == 0);
+	assert_true(res.read == 1);
+	assert_true(cbor_is_int(number));
+	cbor_decref(&number);
+	assert_null(number);
 }
 
 static void test_incomplete_data(void **state) {
-  number = cbor_load(data2, 1, 0, &res);
-  assert_null(number);
-  assert_true(res.error.code == CBOR_ERR_NOTENOUGHDATA);
+	number = cbor_load(data2, 1, 0, &res);
+	assert_null(number);
+	assert_true(res.error.code == CBOR_ERR_NOTENOUGHDATA);
 }
 
 static void test_short_int(void **state) {
-  number = cbor_load(data2, 3, 0, &res);
-  assert_true(cbor_typeof(number) == CBOR_TYPE_UINT);
-  assert_true(cbor_uint_get_width(number) == CBOR_INT_8);
-  assert_true(cbor_isa_uint(number));
-  assert_false(cbor_isa_negint(number));
-  assert_true(cbor_get_uint8(number) == 255);
-  assert_true(res.error.code == 0);
-  assert_true(res.read == 2);
-  assert_true(cbor_is_int(number));
-  cbor_decref(&number);
-  assert_null(number);
+	number = cbor_load(data2, 3, 0, &res);
+	assert_true(cbor_typeof(number) == CBOR_TYPE_UINT);
+	assert_true(cbor_uint_get_width(number) == CBOR_INT_8);
+	assert_true(cbor_isa_uint(number));
+	assert_false(cbor_isa_negint(number));
+	assert_true(cbor_get_uint8(number) == 255);
+	assert_true(res.error.code == 0);
+	assert_true(res.read == 2);
+	assert_true(cbor_is_int(number));
+	cbor_decref(&number);
+	assert_null(number);
 }
 
 static void test_half_int(void **state) {
-  number = cbor_load(data3, 5, 0, &res);
-  assert_true(cbor_typeof(number) == CBOR_TYPE_UINT);
-  assert_true(cbor_uint_get_width(number) == CBOR_INT_16);
-  assert_true(cbor_isa_uint(number));
-  assert_false(cbor_isa_negint(number));
-  assert_true(cbor_get_uint16(number) == 500);
-  assert_true(res.error.code == 0);
-  assert_true(res.read == 3);
-  assert_true(cbor_is_int(number));
-  cbor_decref(&number);
-  assert_null(number);
+	number = cbor_load(data3, 5, 0, &res);
+	assert_true(cbor_typeof(number) == CBOR_TYPE_UINT);
+	assert_true(cbor_uint_get_width(number) == CBOR_INT_16);
+	assert_true(cbor_isa_uint(number));
+	assert_false(cbor_isa_negint(number));
+	assert_true(cbor_get_uint16(number) == 500);
+	assert_true(res.error.code == 0);
+	assert_true(res.read == 3);
+	assert_true(cbor_is_int(number));
+	cbor_decref(&number);
+	assert_null(number);
 }
 
 static void test_int(void **state) {
-  number = cbor_load(data4, 6, 0, &res);
-  assert_true(cbor_typeof(number) == CBOR_TYPE_UINT);
-  assert_true(cbor_uint_get_width(number) == CBOR_INT_32);
-  assert_true(cbor_isa_uint(number));
-  assert_false(cbor_isa_negint(number));
-  assert_true(cbor_get_uint32(number) == 2784428723);
-  assert_true(res.error.code == 0);
-  assert_true(res.read == 5);
-  assert_true(cbor_is_int(number));
-  cbor_decref(&number);
-  assert_null(number);
+	number = cbor_load(data4, 6, 0, &res);
+	assert_true(cbor_typeof(number) == CBOR_TYPE_UINT);
+	assert_true(cbor_uint_get_width(number) == CBOR_INT_32);
+	assert_true(cbor_isa_uint(number));
+	assert_false(cbor_isa_negint(number));
+	assert_true(cbor_get_uint32(number) == 2784428723);
+	assert_true(res.error.code == 0);
+	assert_true(res.read == 5);
+	assert_true(cbor_is_int(number));
+	cbor_decref(&number);
+	assert_null(number);
 }
 
 static void test_long_int(void **state) {
-  number = cbor_load(data5, 10, 0, &res);
-  assert_true(cbor_typeof(number) == CBOR_TYPE_UINT);
-  assert_true(cbor_uint_get_width(number) == CBOR_INT_64);
-  assert_true(cbor_isa_uint(number));
-  assert_false(cbor_isa_negint(number));
-  assert_true(cbor_get_uint64(number) == 11959030306112471731ULL);
-  assert_true(res.error.code == 0);
-  assert_true(res.read == 9);
-  assert_true(cbor_is_int(number));
-  cbor_decref(&number);
-  assert_null(number);
+	number = cbor_load(data5, 10, 0, &res);
+	assert_true(cbor_typeof(number) == CBOR_TYPE_UINT);
+	assert_true(cbor_uint_get_width(number) == CBOR_INT_64);
+	assert_true(cbor_isa_uint(number));
+	assert_false(cbor_isa_negint(number));
+	assert_true(cbor_get_uint64(number) == 11959030306112471731ULL);
+	assert_true(res.error.code == 0);
+	assert_true(res.read == 9);
+	assert_true(cbor_is_int(number));
+	cbor_decref(&number);
+	assert_null(number);
 }
 
 static void test_refcounting(void **state) {
-  number = cbor_load(data5, 10, 0, &res);
-  cbor_incref(number);
-  assert_true(number->refcount == 2);
-  cbor_decref(&number);
-  assert_non_null(number);
-  cbor_decref(&number);
-  assert_null(number);
+	number = cbor_load(data5, 10, 0, &res);
+	cbor_incref(number);
+	assert_true(number->refcount == 2);
+	cbor_decref(&number);
+	assert_non_null(number);
+	cbor_decref(&number);
+	assert_null(number);
 }
 
 static void test_empty_input(void **state) {
-  number = cbor_load(data5, 0, 0, &res);
-  assert_null(number);
-  assert_true(res.error.code == CBOR_ERR_NODATA);
+	number = cbor_load(data5, 0, 0, &res);
+	assert_null(number);
+	assert_true(res.error.code == CBOR_ERR_NODATA);
 }
 
 int main(void) {
-  const UnitTest tests[] = {
-    unit_test(test_very_short_int),
-    unit_test(test_short_int),
-    unit_test(test_half_int),
-    unit_test(test_int),
-    unit_test(test_long_int),
-    unit_test(test_incomplete_data),
-    unit_test(test_refcounting),
-    unit_test(test_empty_input)
-  };
-  return run_tests(tests);
+	const UnitTest tests[] = {
+		unit_test(test_very_short_int),
+		unit_test(test_short_int),
+		unit_test(test_half_int),
+		unit_test(test_int),
+		unit_test(test_long_int),
+		unit_test(test_incomplete_data),
+		unit_test(test_refcounting),
+		unit_test(test_empty_input)
+	};
+	return run_tests(tests);
 }
--- test/type_1_test.c
+++ test/type_1_test.c
@@ -18,83 +18,83 @@ unsigned char data5[] = { 0x3b, 0xa5, 0xf7, 0x02, 0xb3, 0xa5, 0xf7, 0x02, 0xb3, 
 
 
 static void test_very_short_int(void **state) {
-  number = cbor_load(data1, 2, 0, &res);
-  assert_true(cbor_typeof(number) == CBOR_TYPE_NEGINT);
-  assert_true(cbor_uint_get_width(number) == CBOR_INT_8);
-  assert_false(cbor_isa_uint(number));
-  assert_true(cbor_isa_negint(number));
-  assert_true(cbor_get_uint8(number) == 2);
-  assert_true(res.error.code == 0);
-  assert_true(res.read == 1);
-  assert_true(cbor_is_int(number));
-  cbor_decref(&number);
-  assert_null(number);
+	number = cbor_load(data1, 2, 0, &res);
+	assert_true(cbor_typeof(number) == CBOR_TYPE_NEGINT);
+	assert_true(cbor_uint_get_width(number) == CBOR_INT_8);
+	assert_false(cbor_isa_uint(number));
+	assert_true(cbor_isa_negint(number));
+	assert_true(cbor_get_uint8(number) == 2);
+	assert_true(res.error.code == 0);
+	assert_true(res.read == 1);
+	assert_true(cbor_is_int(number));
+	cbor_decref(&number);
+	assert_null(number);
 }
 
 static void test_short_int(void **state) {
-  number = cbor_load(data2, 3, 0, &res);
-  assert_true(cbor_typeof(number) == CBOR_TYPE_NEGINT);
-  assert_true(cbor_uint_get_width(number) == CBOR_INT_8);
-  assert_false(cbor_isa_uint(number));
-  assert_true(cbor_isa_negint(number));
-  assert_true(cbor_get_uint8(number) == 255);
-  assert_true(res.error.code == 0);
-  assert_true(res.read == 2);
-  assert_true(cbor_is_int(number));
-  cbor_decref(&number);
-  assert_null(number);
+	number = cbor_load(data2, 3, 0, &res);
+	assert_true(cbor_typeof(number) == CBOR_TYPE_NEGINT);
+	assert_true(cbor_uint_get_width(number) == CBOR_INT_8);
+	assert_false(cbor_isa_uint(number));
+	assert_true(cbor_isa_negint(number));
+	assert_true(cbor_get_uint8(number) == 255);
+	assert_true(res.error.code == 0);
+	assert_true(res.read == 2);
+	assert_true(cbor_is_int(number));
+	cbor_decref(&number);
+	assert_null(number);
 }
 
 static void test_half_int(void **state) {
-  number = cbor_load(data3, 5, 0, &res);
-  assert_true(cbor_typeof(number) == CBOR_TYPE_NEGINT);
-  assert_true(cbor_uint_get_width(number) == CBOR_INT_16);
-  assert_false(cbor_isa_uint(number));
-  assert_true(cbor_isa_negint(number));
-  assert_true(cbor_get_uint16(number) == 500);
-  assert_true(res.error.code == 0);
-  assert_true(res.read == 3);
-  assert_true(cbor_is_int(number));
-  cbor_decref(&number);
-  assert_null(number);
+	number = cbor_load(data3, 5, 0, &res);
+	assert_true(cbor_typeof(number) == CBOR_TYPE_NEGINT);
+	assert_true(cbor_uint_get_width(number) == CBOR_INT_16);
+	assert_false(cbor_isa_uint(number));
+	assert_true(cbor_isa_negint(number));
+	assert_true(cbor_get_uint16(number) == 500);
+	assert_true(res.error.code == 0);
+	assert_true(res.read == 3);
+	assert_true(cbor_is_int(number));
+	cbor_decref(&number);
+	assert_null(number);
 }
 
 static void test_int(void **state) {
-  number = cbor_load(data4, 6, 0, &res);
-  assert_true(cbor_typeof(number) == CBOR_TYPE_NEGINT);
-  assert_true(cbor_uint_get_width(number) == CBOR_INT_32);
-  assert_false(cbor_isa_uint(number));
-  assert_true(cbor_isa_negint(number));
-  assert_true(cbor_get_uint32(number) == 2784428723);
-  assert_true(res.error.code == 0);
-  assert_true(res.read == 5);
-  assert_true(cbor_is_int(number));
-  cbor_decref(&number);
-  assert_null(number);
+	number = cbor_load(data4, 6, 0, &res);
+	assert_true(cbor_typeof(number) == CBOR_TYPE_NEGINT);
+	assert_true(cbor_uint_get_width(number) == CBOR_INT_32);
+	assert_false(cbor_isa_uint(number));
+	assert_true(cbor_isa_negint(number));
+	assert_true(cbor_get_uint32(number) == 2784428723);
+	assert_true(res.error.code == 0);
+	assert_true(res.read == 5);
+	assert_true(cbor_is_int(number));
+	cbor_decref(&number);
+	assert_null(number);
 }
 
 static void test_long_int(void **state) {
-  number = cbor_load(data5, 10, 0, &res);
-  assert_true(cbor_typeof(number) == CBOR_TYPE_NEGINT);
-  assert_true(cbor_uint_get_width(number) == CBOR_INT_64);
-  assert_false(cbor_isa_uint(number));
-  assert_true(cbor_isa_negint(number));
-  assert_true(cbor_get_uint64(number) == 11959030306112471731ULL);
-  assert_true(res.error.code == 0);
-  assert_true(res.read == 9);
-  assert_true(cbor_is_int(number));
-  cbor_decref(&number);
-  assert_null(number);
+	number = cbor_load(data5, 10, 0, &res);
+	assert_true(cbor_typeof(number) == CBOR_TYPE_NEGINT);
+	assert_true(cbor_uint_get_width(number) == CBOR_INT_64);
+	assert_false(cbor_isa_uint(number));
+	assert_true(cbor_isa_negint(number));
+	assert_true(cbor_get_uint64(number) == 11959030306112471731ULL);
+	assert_true(res.error.code == 0);
+	assert_true(res.read == 9);
+	assert_true(cbor_is_int(number));
+	cbor_decref(&number);
+	assert_null(number);
 }
 
 
 int main(void) {
-  const UnitTest tests[] = {
-    unit_test(test_very_short_int),
-    unit_test(test_short_int),
-    unit_test(test_half_int),
-    unit_test(test_int),
-    unit_test(test_long_int)
-  };
-  return run_tests(tests);
+	const UnitTest tests[] = {
+		unit_test(test_very_short_int),
+		unit_test(test_short_int),
+		unit_test(test_half_int),
+		unit_test(test_int),
+		unit_test(test_long_int)
+	};
+	return run_tests(tests);
 }
