5eca0bae4db2ba97d7e2d70a7b67b3543ea5f5f0
PJK <me@pavelkalvoda.com>
2014-10-19 21:45:34 +0200

    Draft serializers API

--- src/cbor.h
+++ src/cbor.h
@@ -285,6 +285,16 @@ size_t cbor_encode_float(float, unsigned char *, size_t);
 size_t cbor_encode_double(double, unsigned char *, size_t);
 size_t cbor_encode_break(unsigned char *, size_t);
 
+size_t cbor_serialize(cbor_item_t *, unsigned char *, size_t);
+size_t cbor_serialize_uint(cbor_item_t *, unsigned char *, size_t);
+size_t cbor_serialize_negint(cbor_item_t *, unsigned char *, size_t);
+size_t cbor_serialize_bytestring(cbor_item_t *, unsigned char *, size_t);
+size_t cbor_serialize_string(cbor_item_t *, unsigned char *, size_t);
+size_t cbor_serialize_array(cbor_item_t *, unsigned char *, size_t);
+size_t cbor_serialize_map(cbor_item_t *, unsigned char *, size_t);
+size_t cbor_serialize_tag(cbor_item_t *, unsigned char *, size_t);
+size_t cbor_serialize_float_ctrl(cbor_item_t *, unsigned char *, size_t);
+
 void cbor_incref(cbor_item_t * item);
 void cbor_decref(cbor_item_t ** item);
 
