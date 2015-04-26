97d2c44304524999af8860556328ea1c551bea34
PJK <me@pavelkalvoda.com>
2015-04-26 14:40:11 +0200

    Typo

--- buildscript.sh
+++ buildscript.sh
@@ -7,4 +7,4 @@ cmake -DCUSTOM_ALLOC=$CUSTOM_ALLOC -DCMAKE_BUILD_TYPE=Debug $SOURCE
 make
 ctest -V
 ctest -T memcheck | tee memcheck.out
-grep -vE 'Memory Leak - [1-9][0-9]+' memcheck.out
+grep -vE 'Memory Leak - [1-9][0-9]*' memcheck.out
