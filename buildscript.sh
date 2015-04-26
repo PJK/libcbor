82af98168386d2d062bf3ba1beb26a440564ed44
PJK <me@pavelkalvoda.com>
2015-04-26 21:33:38 +0200

    Add more conditions to the tests script

--- buildscript.sh
+++ buildscript.sh
@@ -7,6 +7,6 @@ cmake -DCUSTOM_ALLOC=$CUSTOM_ALLOC -DCMAKE_BUILD_TYPE=Debug $SOURCE
 make
 ctest -V
 ctest -T memcheck | tee memcheck.out
-if grep -q 'Memory Leak' memcheck.out; then
+if grep -q 'Memory Leak\|IPW\|Uninitialized Memory Conditional' memcheck.out; then
     exit 1
 fi
