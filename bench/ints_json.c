97f0323481716ab51872a4076438f00666cab0ca
PJK <me@pavelkalvoda.com>
2015-04-27 21:17:31 +0200

    Add license notice

--- bench/ints_json.c
+++ bench/ints_json.c
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2014-2015 Pavel Kalvoda <me@pavelkalvoda.com>
+ *
+ * libcbor is free software; you can redistribute it and/or modify
+ * it under the terms of the MIT license. See LICENSE for details.
+ */
+
 
 #include <jansson.h>
 
