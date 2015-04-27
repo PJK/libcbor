db3b78b5d38ebebbf6e8a071c18c638b3ca88a8e
PJK <me@pavelkalvoda.com>
2015-04-27 18:18:28 +0200

    New header structure: maps

--- src/cbor/maps.h
+++ src/cbor/maps.h
@@ -8,4 +8,23 @@
 #ifndef LIBCBOR_MAPS_H
 #define LIBCBOR_MAPS_H
 
+#include "common.h"
+
+/*
+* ============================================================================
+* Map manipulation
+* ============================================================================
+*/
+
+size_t cbor_map_size(const cbor_item_t * item);
+cbor_item_t * cbor_new_definite_map(const size_t size);
+cbor_item_t * cbor_new_indefinite_map();
+cbor_item_t * cbor_map_add(cbor_item_t * item, struct cbor_pair pair);
+cbor_item_t * cbor_map_add_key(cbor_item_t * item, cbor_item_t * key);
+
+bool cbor_map_is_definite(const cbor_item_t * item);
+bool cbor_map_is_indefinite(const cbor_item_t * item);
+struct cbor_pair * cbor_map_handle(const cbor_item_t * item);
+
+
 #endif //LIBCBOR_MAPS_H
