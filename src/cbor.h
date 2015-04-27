c8aff42bf14fd83f0b788ada0f998c2f0338236a
PJK <me@pavelkalvoda.com>
2015-04-27 18:23:17 +0200

    New header structure: ints

--- src/cbor.h
+++ src/cbor.h
@@ -171,37 +171,7 @@ size_t cbor_serialize_float_ctrl(const cbor_item_t *, unsigned char *, size_t);
 
 
 
-/*
-* ============================================================================
-* Integer (uints and negints) manipulation
-* ============================================================================
-*/
 
-uint8_t cbor_get_uint8(const cbor_item_t * item);
-uint16_t cbor_get_uint16(const cbor_item_t * item);
-uint32_t cbor_get_uint32(const cbor_item_t * item);
-uint64_t cbor_get_uint64(const cbor_item_t * item);
-
-void cbor_set_uint8(cbor_item_t * item, uint8_t value);
-void cbor_set_uint16(cbor_item_t * item, uint16_t value);
-void cbor_set_uint32(cbor_item_t * item, uint32_t value);
-void cbor_set_uint64(cbor_item_t * item, uint64_t value);
-
-cbor_int_width cbor_int_get_width(const cbor_item_t * item);
-
-void cbor_mark_uint(cbor_item_t * item);
-void cbor_mark_negint(cbor_item_t * item);
-
-cbor_item_t * cbor_new_int8();
-cbor_item_t * cbor_new_int16();
-cbor_item_t * cbor_new_int32();
-cbor_item_t * cbor_new_int64();
-
-/* Build initialized integers - new + set_uint + mark_uint */
-cbor_item_t * cbor_build_uint8(uint8_t value);
-cbor_item_t * cbor_build_uint16(uint16_t value);
-cbor_item_t * cbor_build_uint32(uint32_t value);
-cbor_item_t * cbor_build_uint64(uint64_t value);
 
 
 
