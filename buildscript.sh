d8b39dfec9a7692abccea18178425b3b3a4e08cc
PJK <me@pavelkalvoda.com>
2015-04-28 15:14:26 +0200

    Enable coveralls for CI, take 3

--- buildscript.sh
+++ buildscript.sh
@@ -7,9 +7,11 @@ cmake -DCUSTOM_ALLOC=$CUSTOM_ALLOC -DCMAKE_BUILD_TYPE=Debug $SOURCE
 make
 ctest -V
 ctest -T memcheck | tee memcheck.out
-if grep -q 'Memory Leak\|IPW\|Uninitialized Memory Conditional' memcheck.out; then
+if grep -q 'Memory Leak\|IPW\|Uninitialized Memory Conditional\|Uninitialized Memory Read' memcheck.out; then
     exit 1
 fi
+
+env
 
 lcov --compat-libtool --directory . --capture --output-file coverage.info
 coveralls-lcov coverage.info
