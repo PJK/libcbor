108958a69d5f746356c5f86af8f39e6dad2ed7ca
PJK <me@pavelkalvoda.com>
2015-04-29 00:12:49 +0200

    CI code coverage: I give up :(

--- buildscript.sh
+++ buildscript.sh
@@ -1,19 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-if [ "$CC" == "gcc" ]; then
-    cmake $SOURCE -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=gcc -DCOVERAGE=ON -DCUSTOM_ALLOC=$CUSTOM_ALLOC
-else
-    cmake -DCUSTOM_ALLOC=$CUSTOM_ALLOC -DCMAKE_BUILD_TYPE=Debug -DCOVERAGE=ON $SOURCE
-fi
-
+cmake $SOURCE -DCUSTOM_ALLOC=$CUSTOM_ALLOC -DCMAKE_BUILD_TYPE=Debug
 make VERBOSE=1
 ctest -V
-
-if [ "$CC" == "gcc" ]; then
-    lcov --capture --directory $(pwd) --output-file coverage.info
-    coveralls-lcov coverage.info
-fi
 
 ctest -T memcheck | tee memcheck.out
 if grep -q 'Memory Leak\|IPW\|Uninitialized Memory Conditional\|Uninitialized Memory Read' memcheck.out; then
