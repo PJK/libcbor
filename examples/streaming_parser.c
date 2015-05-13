ee864fd0e397d8a7671b591de39ec228e314e2f0
PJK <me@pavelkalvoda.com>
2015-05-13 14:32:15 +0200

    Tiny fixes, getting ready for the next release

--- examples/streaming_parser.c
+++ examples/streaming_parser.c
@@ -12,15 +12,24 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+ * Illustrates how one might skim through a map (which is assumed to have
+ * string keys and values only), looking for the value of a specific key
+ *
+ * Use the examples/data/map.cbor input to test this.
+ */
+
 const char * key = "a secret key";
 bool key_found = false;
 
 void find_string(void * _ctx, cbor_data buffer, size_t len)
 {
-	if (key_found)
+	if (key_found) {
 		printf("Found the value: %*s\n", (int) len, buffer);
-	else if (len == strlen(key))
-		key_found = memcmp(key, buffer, len);
+		key_found = false;
+	} else if (len == strlen(key)) {
+		key_found = (memcmp(key, buffer, len) == 0);
+	}
 }
 
 int main(int argc, char * argv[])
@@ -28,14 +37,17 @@ int main(int argc, char * argv[])
 	int fd = open(argv[1], O_RDONLY);
 	struct stat info;
 	fstat(fd, &info);
-	unsigned char * buffer = mmap(NULL, info.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+	unsigned char * buffer = mmap(NULL, info.st_size, PROT_READ,
+	                              MAP_PRIVATE, fd, 0);
 
 	struct cbor_callbacks callbacks = cbor_empty_callbacks;
 	struct cbor_decoder_result decode_result;
 	size_t bytes_read = 0;
 	callbacks.string = find_string;
 	while (bytes_read < info.st_size) {
-		decode_result = cbor_stream_decode(buffer, info.st_size - bytes_read, &callbacks, NULL);
+		decode_result = cbor_stream_decode(buffer + bytes_read,
+		                                   info.st_size - bytes_read,
+		                                   &callbacks, NULL);
 		bytes_read += decode_result.read;
 	}
 }
