e3105cf6c404563cab0d4e19e6cdc93b89b4e671
dgrigsby <david.grigsby@disney.com>
2015-12-14 16:51:42 -0800

    Change to runtime check (which will likely be optimized out). The previous #if method didn't compile on LLVM 7.0.

--- src/cbor/internal/encoders.c
+++ src/cbor/internal/encoders.c
@@ -37,14 +37,14 @@ size_t _cbor_encode_uint16(uint16_t value, unsigned char *buffer, size_t buffer_
 		buffer[0] = 0x19 + offset;
 
 #ifdef HAVE_ENDIAN_H
-	*(uint16_t *) &buffer[1] = htobe16(value);
+        *(uint16_t *) &buffer[1] = htobe16(value);
 #else
-	#if IS_BIG_ENDIAN
-		*(uint16_t *) &buffer[1] = value;
-	#else
-		buffer[1] = value >> 8;
-		buffer[2] = value;
-	#endif
+        if IS_BIG_ENDIAN
+            *(uint16_t *) &buffer[1] = value;
+        else {
+            buffer[1] = value >> 8;
+            buffer[2] = value;
+        }
 #endif
 
 		return 3;
@@ -60,14 +60,14 @@ size_t _cbor_encode_uint32(uint32_t value, unsigned char *buffer, size_t buffer_
 #ifdef HAVE_ENDIAN_H
 		*(uint32_t *) &buffer[1] = htobe32(value);
 #else
-	#if IS_BIG_ENDIAN
-		*(uint32_t *) &buffer[1] = value;
-	#else
-		buffer[1] = value >> 24;
-		buffer[2] = value >> 16;
-		buffer[3] = value >> 8;
-		buffer[4] = value;
-	#endif
+        if IS_BIG_ENDIAN
+            *(uint32_t *) &buffer[1] = value;
+        else {
+            buffer[1] = value >> 24;
+            buffer[2] = value >> 16;
+            buffer[3] = value >> 8;
+            buffer[4] = value;
+        }
 #endif
 
 		return 5;
@@ -83,18 +83,18 @@ size_t _cbor_encode_uint64(uint64_t value, unsigned char *buffer, size_t buffer_
 #ifdef HAVE_ENDIAN_H
 		*(uint64_t *) &buffer[1] = htobe64(value);
 #else
-	#if IS_BIG_ENDIAN
-		*(uint64_t *) &buffer[1] = value;
-	#else
-		buffer[1] = value >> 56;
-		buffer[2] = value >> 48;
-		buffer[3] = value >> 40;
-		buffer[4] = value >> 32;
-		buffer[5] = value >> 24;
-		buffer[6] = value >> 16;
-		buffer[7] = value >> 8;
-		buffer[8] = value;
-	#endif
+        if IS_BIG_ENDIAN
+            *(uint64_t *) &buffer[1] = value;
+        else {
+            buffer[1] = value >> 56;
+            buffer[2] = value >> 48;
+            buffer[3] = value >> 40;
+            buffer[4] = value >> 32;
+            buffer[5] = value >> 24;
+            buffer[6] = value >> 16;
+            buffer[7] = value >> 8;
+            buffer[8] = value;
+        }
 #endif
 
 		return 9;
