cf764f7a4a48cd574451e570731963a15c16c60d
PJK <me@pavelkalvoda.com>
2015-12-25 15:05:10 +0100

    Export PP flag

--- examples/hello.c
+++ examples/hello.c
@@ -12,5 +12,6 @@ int main(int argc, char * argv[])
 {
 	printf("Hello from libcbor %s\n", CBOR_VERSION);
 	printf("Custom allocation support: %s\n", CBOR_CUSTOM_ALLOC ? "yes" : "no");
-	printf("Buffer growth factor: %f\n", CBOR_BUFFER_GROWTH);
+	printf("Pretty-printer support: %s\n", CBOR_PRETTY_PRINTER ? "yes" : "no");
+	printf("Buffer growth factor: %f\n", (float) CBOR_BUFFER_GROWTH);
 }
