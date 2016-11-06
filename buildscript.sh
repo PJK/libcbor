8dce3569344f41e0f2d3dc23243629a32247c34c
PJK <me@pavelkalvoda.com>
2016-11-06 19:55:58 +0100

    Trying container CI v23

--- buildscript.sh
+++ buildscript.sh
@@ -8,7 +8,7 @@ cd $PREVD
 
 cmake $SOURCE -DCMAKE_CUSTOM_ALLOC=ON -DCMAKE_BUILD_TYPE=Debug -DWITH_TESTS=ON -DCMAKE_PREFIX_PATH=$HOME/usr/local
 make VERBOSE=1
-ctest -V
+ctest -VV
 
 ctest -T memcheck | tee memcheck.out
 if grep -q 'Memory Leak\|IPW\|Uninitialized Memory Conditional\|Uninitialized Memory Read' memcheck.out; then
