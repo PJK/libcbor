a087683ab97cf9ecbbd19e15b2146bd4ee949815
PJK <me@pavelkalvoda.com>
2016-11-06 20:03:53 +0100

    Trying container CI v25

--- buildscript.sh
+++ buildscript.sh
@@ -8,6 +8,8 @@ cd $PREVD
 
 cmake $SOURCE -DCMAKE_CUSTOM_ALLOC=ON -DCMAKE_BUILD_TYPE=Debug -DWITH_TESTS=ON -DCMAKE_PREFIX_PATH=$HOME/usr/local
 make VERBOSE=1
+./test/fuzz_test 
+
 ctest -VV
 
 ctest -T memcheck | tee memcheck.out
