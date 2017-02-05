6c4df7f27461a3eafc3f9689e14ce2ffda654ee0
PJK <me@pavelkalvoda.com>
2017-02-05 17:21:09 +0100

    Update release script to correctly copy HTML ref

--- release.sh
+++ release.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -x
+set -e
 
 DIR=$(pwd) # Assume runing from this dir
 OUTDIR=$(mktemp -d)
-cd doc
+pushd doc
 make clean
-cd ..
+popd
 doxygen
 cd doc
 make html
@@ -13,19 +14,17 @@ cd build
 
 cp -r html libcbor_docs_html
 tar -zcf libcbor_docs.tar.gz libcbor_docs_html
-rm -rf libcbor_docs_html
 
 cp -r doxygen/html libcbor_api_docs_html
+cp -r doxygen/html $DIR/docs/doxygen
 tar -zcf libcbor_api_docs.tar.gz libcbor_api_docs_html
-rm -rf libcbor_api_docs_html
 
 mv libcbor_docs.tar.gz libcbor_api_docs.tar.gz $OUTDIR
 
 cd $OUTDIR
 
-cmake $DIR -DCMAKE_BUILD_TYPE=Release
-make
-ctest
-sudo make package
+#cmake $DIR -DCMAKE_BUILD_TYPE=Release -DWITH_TESTS=ON
+#make
+#ctest
 
 echo Relase ready in $OUTDIR
