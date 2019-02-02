d2b777e064d15553d04d52deaa85e045f5feafd1
Pavel Kalvoda <me@pavelkalvoda.com>
2019-02-02 14:10:19 +0100

    Testing travis c-f :( #2

--- clang-format.sh
+++ clang-format.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-clang-format -style=file -i **/*.c **/*.h **/*.cpp
+# TravisCI workaround to use new clang-format while avoiding painful aliasing
+# into the subshell
+if which clang-format-8; then
+    clang-format-8 -style=file -i **/*.c **/*.h **/*.cpp
+else
+    clang-format -style=file -i **/*.c **/*.h **/*.cpp
+fi
+
