1e079ae907bc6d002bb8801f2d094c76c57332d7
Nicolas Mora <github@babelouest.org>
2019-07-25 09:29:38 -0400

    clang-format: output verbose

--- clang-format.sh
+++ clang-format.sh
@@ -3,8 +3,8 @@
 # TravisCI workaround to use new clang-format while avoiding painful aliasing
 # into the subshell
 if which clang-format-8; then
-    clang-format-8 -style=file -i **/*.c **/*.h **/*.cpp
+    clang-format-8 --verbose -style=file -i **/*.c **/*.h **/*.cpp
 else
-    clang-format -style=file -i **/*.c **/*.h **/*.cpp
+    clang-format --verbose -style=file -i **/*.c **/*.h **/*.cpp
 fi
 
