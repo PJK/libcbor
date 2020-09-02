da0ea38b7762a3bcf90ef4b394356549c2f982dd
Pavel Kalvoda <me@pavelkalvoda.com>
2020-09-03 01:45:26 +0200

    Fix cbor_tag_item usage in cbor_describe

--- src/cbor.c
+++ src/cbor.c
@@ -317,7 +317,7 @@ static void _cbor_nested_describe(cbor_item_t *item, FILE *out, int indent) {
     case CBOR_TYPE_TAG: {
       fprintf(out, "%*s[CBOR_TYPE_TAG] ", indent, " ");
       fprintf(out, "Value: %" PRIu64 "\n", cbor_tag_value(item));
-      _cbor_nested_describe(cbor_tag_item(item), out, indent + 4);
+      _cbor_nested_describe(cbor_move(cbor_tag_item(item)), out, indent + 4);
       break;
     };
     case CBOR_TYPE_FLOAT_CTRL: {
