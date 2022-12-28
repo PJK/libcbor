f036ef0f637d9377035f2ca2c572d3df104c8dc9
Pavel Kalvoda <me@pavelkalvoda.com>
2022-12-28 12:36:56 +0100

    Better serialization: cbor_serialized_size for strings

--- src/cbor/serialization.c
+++ src/cbor/serialization.c
@@ -73,7 +73,7 @@ size_t cbor_serialized_size(const cbor_item_t *item) {
         case CBOR_INT_64:
           return 9;
       }
-    case CBOR_TYPE_BYTESTRING:
+    case CBOR_TYPE_BYTESTRING: {
       if (cbor_bytestring_is_definite(item)) {
         return _cbor_safe_signaling_add(
             _cbor_encoded_header_size(cbor_bytestring_length(item)),
@@ -87,9 +87,22 @@ size_t cbor_serialized_size(const cbor_item_t *item) {
             indef_bytestring_size, cbor_serialized_size(chunks[i]));
       }
       return indef_bytestring_size;
-    case CBOR_TYPE_STRING:
-      // TODO
-      return 0;
+    }
+    case CBOR_TYPE_STRING: {
+      if (cbor_string_is_definite(item)) {
+        return _cbor_safe_signaling_add(
+            _cbor_encoded_header_size(cbor_string_length(item)),
+            cbor_string_length(item));
+      }
+      size_t chunk_count = cbor_string_chunk_count(item);
+      size_t indef_string_size = 2;  // Leading byte + break
+      cbor_item_t **chunks = cbor_string_chunks_handle(item);
+      for (size_t i = 0; i < chunk_count; i++) {
+        indef_string_size = _cbor_safe_signaling_add(
+            indef_string_size, cbor_serialized_size(chunks[i]));
+      }
+      return indef_string_size;
+    }
     case CBOR_TYPE_ARRAY:
       // TODO
       return 0;
